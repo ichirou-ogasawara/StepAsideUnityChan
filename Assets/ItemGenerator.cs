83103e1e7743b1a6f813c81db8b7930bf9e88618
ichirou-ogasawara <90944147+ichirou-ogasawara@users.noreply.github.com>
2021-10-09 17:24:31 +0900

    アイテム生成の修正
    ゴールの奥にもアイテムが生成されていたので、ゴール地点からアイテム生成範囲(60m)のぶん手前を最終生成ラインとした　変数にしたので範囲の数値を変えても問題なし

--- Assets/ItemGenerator.cs
+++ Assets/ItemGenerator.cs
@@ -11,9 +11,10 @@ public class ItemGenerator : MonoBehaviour
 
     private int startPos = 20;
     private int goalPos = 360;
+    private int generateRange = 60; //前方のアイテム生成範囲
     private float posRange = 3.4f;
     private float unitychanPos;
-    private int lastGeneratePos; //直近で通過した生成位置
+    private float lastGeneratePos; //直近で通過した生成位置
 
     // Start is called before the first frame update
     void Start()
@@ -27,7 +28,7 @@ public class ItemGenerator : MonoBehaviour
     {
         this.unitychanPos = this.unitychan.transform.position.z; //unitychanのz座標を取得
         
-        if ((unitychanPos - lastGeneratePos) >= 15) //直近の生成位置との距離が15mになると
+        if ((unitychanPos - lastGeneratePos) > 15 && (unitychanPos < goalPos - generateRange)) //直近の生成位置との距離が15mになると、最終生成ラインまで
         {
             int num = Random.Range (1, 11); //どのアイテムを出すのかをランダムに設定
             if (num <= 2)
@@ -36,7 +37,7 @@ public class ItemGenerator : MonoBehaviour
                 for (float j = -1; j <= 1; j += 0.4f)
                 {
                     GameObject cone = Instantiate (conePrefab);
-                    cone.transform.position = new Vector3 (4 * j, cone.transform.position.y, unitychanPos + 60); //60m先に生成
+                    cone.transform.position = new Vector3 (4 * j, cone.transform.position.y, unitychanPos + generateRange); //60m先に生成
                 }
             }
             else
@@ -48,19 +49,16 @@ public class ItemGenerator : MonoBehaviour
                     if (1 <= item && item <= 6)
                     {
                         GameObject coin = Instantiate (coinPrefab);
-                        coin.transform.position = new Vector3 (posRange * j, coin.transform.position.y, unitychanPos + 60 + offsetZ);
+                        coin.transform.position = new Vector3 (posRange * j, coin.transform.position.y, unitychanPos + generateRange + offsetZ);
                     }
                     else if (7 <= item && item <= 9)
                     {
                         GameObject car = Instantiate (carPrefab);
-                        car.transform.position = new Vector3 (posRange * j, car.transform.position.y, unitychanPos + 60 + offsetZ);
+                        car.transform.position = new Vector3 (posRange * j, car.transform.position.y, unitychanPos + generateRange + offsetZ);
                     }
                 }
             }
-            if (lastGeneratePos < goalPos) //生成位置がゴールを超えるまで
-            {
-                this.lastGeneratePos += 15; //生成位置を15m先に更新
-            }    
+            this.lastGeneratePos = unitychanPos; //生成位置にユニティちゃんの座標を代入
         }
     }
 }
